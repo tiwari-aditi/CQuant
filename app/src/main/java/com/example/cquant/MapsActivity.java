41293cbe154b59531a6243cf6946e1b9f8252953
HARSH <theharsh1210@gmail.com>
2022-11-04 16:21:09 +0530

    emission done

--- app/src/main/java/com/example/cquant/MapsActivity.java
+++ app/src/main/java/com/example/cquant/MapsActivity.java
@@ -1,21 +1,59 @@
 package com.example.cquant;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.cquant.databinding.ActivityMapsBinding;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.cquant.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMarkerDragListener,GoogleMap.OnMapLongClickListener{
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private GoogleApiClient client;
+    private LocationRequest locationRequest;
+    private Location lastLocation;
+    private Marker currentLocationMarker;
+    public static final int REQUEST_LOCATION_CODE = 99;
+    double latitude,longitude;
+    double end_latitude,end_longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +62,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+                    if (ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if(client == null)
+                        {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 
     /**
@@ -44,8 +116,161 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+
+        }
+        //mMap.setOnMarkerClickListener(this);
+        //mMap.setOnMarkerDragListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    protected synchronized void buildGoogleApiClient()
+    {
+        client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        client.connect();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lastLocation = location;
+
+        if(currentLocationMarker != null)
+        {
+            currentLocationMarker.remove();
+        }
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        LatLng latLng =  new LatLng(location.getLatitude(),location.getLongitude());
+        MarkerOptions markerOption = new MarkerOptions();
+        markerOption.position(latLng);
+        markerOption.title("Current Location");
+        markerOption.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        currentLocationMarker = mMap.addMarker(markerOption);
+
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(0));
+
+        if(client != null)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(client,locationRequest,this);
+        }
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED )
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(client,locationRequest,this);
+        }
+
+    }
+
+    public boolean checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION_CODE);
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION_CODE);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void onClick(View v)
+    {
+        if(v.getId()==R.id.to_button)
+        {
+            mMap.clear();
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(new LatLng(latitude,longitude));
+            markerOptions.position(new LatLng(end_latitude,end_longitude));
+            markerOptions.title("Destination");
+
+            float[] result = new float[10];
+            Location.distanceBetween(latitude,longitude,end_latitude,end_longitude,result);
+            Float distance = result[0];
+            Float dis = distance/1000;
+            markerOptions.snippet("Distance = "+dis+"Km");
+            mMap.addMarker(markerOptions);
+            TextView num_dis = findViewById(R.id.num);
+            num_dis.setText(String.format("%.3fKm",dis));
+
+
+
+            Button co2_calc = findViewById(R.id.calculator);
+            co2_calc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(),emission.class);
+                    intent.putExtra("msg_dis",dis);
+                    startActivity(intent);
+                }
+            });
+
+
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+
+
+        //marker.setDraggable(true);
+        return false;
+    }
+
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker) {
+
+
+    }
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker) {
+
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+        //end_latitude = marker.getPosition().latitude;
+        //end_longitude = marker.getPosition().longitude;
+    }
+
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng nlatLng) {
+        mMap.addMarker(new MarkerOptions().position(nlatLng));
+        end_longitude = nlatLng.longitude;
+        end_latitude = nlatLng.latitude;
     }
 }
---
+++ app/src/main/java/com/example/cquant/emission.java
@@ -0,0 +1,206 @@
+package com.example.cquant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.w3c.dom.Text;
+
+public class emission extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emission);
+
+        Spinner vehicle = findViewById(R.id.transport);
+        String[] items = new String[]{"2W(Two-Wheeler)", "3W(Three-Wheeler)", "4W(Four-Wheeler)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, items);
+        vehicle.setAdapter(adapter);
+
+
+
+
+
+        Intent intent = getIntent();
+        float d = intent.getFloatExtra("msg_dis",0);
+        String str = String.format("%.3fKm",d);
+
+
+        TextView distance = findViewById(R.id.dist);
+        distance.setText(str);
+
+        vehicle.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View view, int i, long l) {
+                String selected_vehi = vehicle.getSelectedItem().toString();
+
+                Spinner Fuel = findViewById(R.id.fuel);
+                String[] item = new String[]{"Petrol", "Diesel"};
+                ArrayAdapter<String> adapter1 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, item);
+                Fuel.setAdapter(adapter1);
+
+
+
+
+
+                Fuel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapter1, View view, int i, long l) {
+                        String selected_fuel = Fuel.getSelectedItem().toString();
+
+
+                        Button calc_btn = findViewById(R.id.btn_calc);
+
+
+
+
+
+                        if(selected_vehi.equals("2W(Two-Wheeler)") && selected_fuel.equals("Petrol"))
+                        {
+
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                    double calc_emission =  39.04 * d;
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(String.format("%.3f gram",calc_emission));
+
+
+
+                                }
+                            });
+
+
+                        }
+                        else if(selected_vehi.equals("3W(Three-Wheeler)") && selected_fuel.equals("Petrol"))
+                        {
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    double calc_emission =  113.5 * d;
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(String.format("%.3f gram",calc_emission));
+
+
+
+                                }
+                            });
+
+                        }
+                        else if(selected_vehi.equals("3W(Three-Wheeler)") && selected_fuel.equals("Diesel"))
+                        {
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    double calc_emission =  132.2 * d;
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(String.format("%.3f gram",calc_emission));
+
+
+
+                                }
+                            });
+
+                        }
+                        else if(selected_vehi.equals("4W(Four-Wheeler)") && selected_fuel.equals("Petrol"))
+                        {
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    double calc_emission =  179.94 * d;
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(String.format("%.3f gram",calc_emission));
+
+
+
+                                }
+                            });
+
+                        }
+                        else if(selected_vehi.equals("4W(Four-Wheeler)") && selected_fuel.equals("Diesel"))
+                        {
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    double calc_emission =  173.58 * d;
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(String.format("%.3f gram",calc_emission));
+
+
+
+                                }
+                            });
+
+                        }
+                        else
+                        {
+
+                            calc_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    String s = "*Something went wrong";
+                                    TextView calc_CO2 = findViewById(R.id.text_co2);
+                                    calc_CO2.setText(s);
+
+
+
+                                }
+                            });
+                        }
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
