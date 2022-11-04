d7116cf703897281f422d89dfbfef2478e29d1de
HARSH <theharsh1210@gmail.com>
2022-11-05 04:43:21 +0530

    leaderboard

--- app/src/main/java/com/example/cquant/sign_up.java
+++ app/src/main/java/com/example/cquant/sign_up.java
@@ -3,15 +3,12 @@ package com.example.cquant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
-public class NGOs extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ngos);
-
+        setContentView(R.layout.activity_sign_up);
     }
 }
