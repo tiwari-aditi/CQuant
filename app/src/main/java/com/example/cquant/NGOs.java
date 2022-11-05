a7336a083e4ee1842d0e4c009c1770b90bb760df
tiwari-aditi <106127744+tiwari-aditi@users.noreply.github.com>
2022-11-05 07:42:44 +0530

    ...

--- app/src/main/java/com/example/cquant/NGOs.java
+++ app/src/main/java/com/example/cquant/NGOs.java
@@ -2,8 +2,11 @@ package com.example.cquant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class NGOs extends AppCompatActivity {
@@ -12,6 +15,15 @@ public class NGOs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ngos);
+
+        Button join = findViewById(R.id.joinSatpudaBtn);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(),JoinNGO.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 }
